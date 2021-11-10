dd015ac0e4326b57b14e99fadfbaf0acda3a476f
Jan Kelemen <jan@jankelemen.net>
2021-11-10 08:29:26 +0100

    Update touchpad.sh

--- scripts/.scripts/touchpad.sh
+++ scripts/.scripts/touchpad.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-#id=$(xinput list | awk '$0 ~ /Touchpad/ {print substr($6,4)}')
-#isOn=$([ $(xinput list-props $id | awk '$0 ~ /Device Enabled/ {print $4}') = "0" ] && echo "off" || echo "on")
-#
-#if [ "$1" = "-i" ]; then
-#	echo $isOn
-#else
-#	if [ "$isOn" = "off" ]; then
-#		xinput set-prop $id "Device Enrabled" 1 && echo "touchpad enabled"
-#	else
-#		xinput set-prop $id "Device Enabled" 0 && echo "touchpad disabled"
-#	fi
-#fi
-
 device="$(xinput list | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o | head -n1)"
 
 if [ "$1" = "-i" ]; then
